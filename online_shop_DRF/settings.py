c4b89b46f73e3f87fb7f726468b3689ecc126c72
hoseinoori <hosein86noori@gmail.com>
2026-06-30 19:26:56 +0330

    check-1

--- online_shop_DRF/settings.py
+++ online_shop_DRF/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders', 
 
     # packages
     'rest_framework',
@@ -53,6 +54,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -143,3 +145,5 @@ SPECTACULAR_SETTINGS = {
     'VERSION': '1.0',
     'SERVE_INCLUDE_SCHEMA': False,
 }
+
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
